7ff443a66ce66588b60ca2e05a4b421a6343f061
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 06:54:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,9 +11,9 @@ def nyc_pigeon_organizer(data)
     attr.each do |desc, names|
       names.each do |name|
         if hash[name][category] #category found add desc
-          hash[name][category] << desc
+          hash[name][category] << desc.to_s
         else #add {category => [desc]}
-          hash[name][category] = [desc]
+          hash[name][category] = [desc.to_s]
         end
       end
     end
